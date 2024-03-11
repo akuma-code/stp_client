9d65024bfd29d3ba0367cdf3c76fa05638361fc8
akuma-code <akumapl86@gmail.com>
2024-03-11 19:04:44 +0300

    sync

--- src/Components/StpTable/FormulaParser.ts
+++ src/Components/StpTable/FormulaParser.ts
@@ -1,5 +1,8 @@
 import { _log } from "../../Helpers/helpersFns"
 const hasTriplex = (parts: string[]) => parts.some(name => [`3.3.1`, `4.4.1`].includes(name))
+
+
+const triplex = [`3.3.1`, `4.4.1`]
 const glasses = [
     'TopN',
     'Эл',
@@ -8,27 +11,32 @@ const glasses = [
     'FhBr',
     'FhGr',
     '(зак)',
-]
+    `3.3.1`,
+    `4.4.1`,
+    'Ar',
+    'TGI',
+    '(Ч)',
+] as const
 const ramki = [
     'Ar',
     'TGI',
     '(Ч)',
 ]
-const triplex = [`3.3.1`, `4.4.1`]
+const tagCondition = (tags: typeof glasses[number][]) => {
+    if (tags.some(t => t === 'TopN')) _log('energy')
+
+}
+
 export const parse_name = (stp_name: string) => {
     console.clear()
-    if (triplex.some(t => stp_name.includes(t))) {
-
-    }
+    tagCondition(['Ar',])
+    const tags = glasses.filter(f => stp_name.includes(f))
     const name_parts = stp_name.split('-').filter(p => triplex.indexOf(p) < 0)
     const gls = name_parts.filter((n, i) => i % 2 === 0)
     const numbs = name_parts.map(i => Number(i.match(/\d+/g)))
     const strs = name_parts.map(i => i.match(/\D\w+/g))
     _log('numbs: ', ...numbs)
     _log('strs: ', ...strs)
+    _log('tags: ', tags)
 }
 
-const tagCondition = {
-    simple: [],
-
-}
