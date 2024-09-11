fbe68857472160dc8fa4bb0fd6b14e6b1fd1d1dd
akuma-code <akumapl86@gmail.com>
2024-09-11 18:14:46 +0300

    sync

--- src/Routes/Pages/v2/GoogleApiPage.tsx
+++ src/Routes/Pages/v2/GoogleApiPage.tsx
@@ -1,43 +1,57 @@
-import { Button } from '@mui/material'
-import { QueryClient } from '@tanstack/react-query'
-import { LoaderFunctionArgs, useLoaderData, useNavigate } from 'react-router-dom'
-import { api } from '../../../HTTP/mainApi'
-import { _ID, _log } from '../../../Helpers/helpersFns'
-import { TStpData } from '../../../Hooks/useQueryFetch'
-import { apiRoute, proxyRoute } from '../../routePath'
+import { Button } from "@mui/material";
+import { QueryClient } from "@tanstack/react-query";
+import {
+    LoaderFunctionArgs,
+    useLoaderData,
+    useNavigate,
+} from "react-router-dom";
+import { api } from "../../../HTTP/mainApi";
+import { _ID, _log } from "../../../Helpers/helpersFns";
+import { TStpData } from "../../../Hooks/useQueryFetch";
+import { apiRoute, proxyRoute } from "../../routePath";
 
-
-
-export const loader = (queryClient: QueryClient) => async ({ request }: LoaderFunctionArgs) => {
-    const context = await queryClient.ensureQueryData({
-        queryKey: ['google_api'],
-        queryFn: async () => api.get<{ stps: TStpData[], fields: string[] }>(proxyRoute(apiRoute.stp_db))
-    })
-    return context
-
-}
+export const loader =
+    (queryClient: QueryClient) =>
+    async ({ request }: LoaderFunctionArgs) => {
+        const context = await queryClient.ensureQueryData({
+            queryKey: ["google_api"],
+            queryFn: async () =>
+                api.get<{ stps: TStpData[]; fields: string[] }>(
+                    apiRoute.stp_db,
+                    {
+                        headers: {
+                            "Access-Control-Allow-Origin": "*",
+                            "Access-Control-Allow-Methods":
+                                "GET, POST, PUT, DELETE, OPTIONS",
+                            "Access-Control-Allow-Headers":
+                                "Content-Type, Authorization",
+                        },
+                    }
+                ),
+        });
+        return context;
+    };
 
 export default function GoogleApiPage() {
-    const { data, ...rest } = useLoaderData() as Awaited<ReturnType<ReturnType<typeof loader>>>
-    const navigate = useNavigate()
-    _log(rest)
+    const { data, ...rest } = useLoaderData() as Awaited<
+        ReturnType<ReturnType<typeof loader>>
+    >;
+    const navigate = useNavigate();
+    _log(rest);
     return (
         <div>
-            <Button variant='text' onClick={ () => navigate(-1) }>
+            <Button variant="text" onClick={() => navigate(-1)}>
                 Вернуться назад
-                {/* <MuiLink to={ routePaths.v2 }>Back to v2</MuiLink> */ }
+                {/* <MuiLink to={ routePaths.v2 }>Back to v2</MuiLink> */}
             </Button>
-            <p className='p-2'>
-
+            <p className="p-2">
                 Data:
-                { rest.status === 200 ?
-                    data?.stps?.map(s =>
-                        <li key={ _ID() }>{ s.join("-") }</li>
-                    )
-                    :
+                {rest.status === 200 ? (
+                    data?.stps?.map((s) => <li key={_ID()}>{s.join("-")}</li>)
+                ) : (
                     <div>Error!</div>
-                }
+                )}
             </p>
         </div>
-    )
+    );
 }
